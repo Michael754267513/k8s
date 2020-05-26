672c8b2fa5b269aa3e07b26c357a8f2fce425bec
曾浩 <754267513@qq.com>
2020-05-26 17:00:02 +0800

    新增 deployment service namespace 处理段

--- router/router.go
+++ router/router.go
@@ -3,7 +3,10 @@ package router
 import (
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	Deploy "k8s/kubernetes/deployment"
+	Namespace "k8s/kubernetes/namespace"
 	Pod "k8s/kubernetes/pod"
+	Service "k8s/kubernetes/service"
 )
 
 func MiddlewareCORS(r *ghttp.Request) {
@@ -13,11 +16,16 @@ func MiddlewareCORS(r *ghttp.Request) {
 
 func init() {
 	s := g.Server()
-	s.Group("/", func(group *ghttp.RouterGroup) {
-		pod := new(Pod.PodController)
-		podws := new(Pod.PodWSController)
+	s.Group("/kubernetes", func(group *ghttp.RouterGroup) {
 		group.Middleware(MiddlewareCORS) // 跨域处理
-		group.ALL("/kubernetes/pod", pod)
-		group.ALL("/kubernetes/pod", podws)
+		// pod 处理段
+		group.ALL("/pod/", new(Pod.PodController))
+		group.ALL("/pod/", new(Pod.PodWSController))
+		// deployment 处理段
+		group.ALL("/deployment", new(Deploy.DeployMentController))
+		// service 处理段
+		group.ALL("/service", new(Service.ServiceController))
+		// namespace 处理段
+		group.ALL("/namespace", new(Namespace.NameSpaceController))
 	})
 }
